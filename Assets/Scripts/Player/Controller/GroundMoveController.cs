a543429badc3c06ca981fe535be92de5bd6c7cbc
JulianLi515 <lijulian@usc.edu>
2025-02-25 16:09:47 -0800

    111

--- Assets/Scripts/Player/Controller/GroundMoveController.cs
+++ Assets/Scripts/Player/Controller/GroundMoveController.cs
@@ -12,7 +12,18 @@ public class GroundMoveController
 
     public void onHorizontalInput(float Xinput)
     {
-        player.rb.linearVelocity = new Vector2(Xinput*player.HorizontalSpeedGround, player.rb.linearVelocity.y);
+
+        // stand on spear
+        if (player.transform.parent!= null)
+        {
+            Vector2 parentSpeed = player.transform.parent.GetComponent<Rigidbody2D>().linearVelocity;
+            player.rb.linearVelocity = new Vector2(Xinput * player.HorizontalSpeedGround, player.rb.linearVelocity.y) + parentSpeed;
+        }
+        else
+        {
+            player.rb.linearVelocity = new Vector2(Xinput*player.HorizontalSpeedGround, player.rb.linearVelocity.y);
+
+        }
     }
     public void Freeze()
     {
