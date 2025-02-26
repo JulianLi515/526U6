d0c8518ec5ed760b275f6466d30c4ae6241c2053
JulianLi515 <lijulian@usc.edu>
2025-02-25 22:19:50 -0800

    cc

--- Assets/DrpSpearVertical.cs
+++ Assets/DrpSpearVertical.cs
@@ -1,17 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class DrpSpearVertical : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public enum SpearState
+    {
+        InAir,
+        OnGround
+    }
+
+    public SpearState state;
+    public float moveSpeed;
+    public float liveTime;
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        state = SpearState.InAir;
+        rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        StartCoroutine(nameof(DestroySpearVCoroutine));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case SpearState.InAir:
+                rb.linearVelocity = -transform.up * moveSpeed;
+
+                break;
+            case SpearState.OnGround:
+                break;
+        }
     }
 
     //private void OnTriggerExit(Collider other)
@@ -21,4 +44,30 @@ public class DrpSpearVertical : MonoBehaviour
     //        //other.GetComponent<Player>().OnDamage();
     //    }
     //}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log(collision.gameObject.name);
+        if (state == SpearState.OnGround)
+        {
+            return;
+        }
+
+        if (other.CompareTag("Level"))
+        {
+            state = SpearState.OnGround;
+            rb.linearVelocity = Vector2.zero;
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            //rb.bodyType = RigidbodyType2D.Dynamic;
+            //pe.enabled = true;
+            transform.parent = other.transform;
+            //gameObject.layer = LayerMask.NameToLayer("Ground");
+        }
+    }
+
+    IEnumerator DestroySpearVCoroutine()
+    {
+        yield return new WaitForSeconds(liveTime);
+        Destroy(gameObject);
+    }
 }
